ede20d95c6b83eef1aee38695f9252a158ee2be7
bicoscryin <bico970@gmail.com>
2025-12-18 10:59:19 -0500

    removed classifiers

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 from all_imports import *
 from variables import *
 
-# Model Code
+# Transformers
 
 numeric_transformer = Pipeline([
     ("imputer", SimpleImputer(strategy="mean")),   # Fill missing with mean
@@ -12,48 +12,12 @@ categorical_transformer = Pipeline([
     ("encoder", OneHotEncoder(handle_unknown="ignore")) 
 ])
 
-# ColumnTransformer applies the correct transformer to each column set
 preprocessor = ColumnTransformer([
     ("num", numeric_transformer, numeric_features),
     ("cat", categorical_transformer, categorical_features)
 ])
 
-
-rfc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", RandomForestClassifier(random_state=42))
-])
-
-dtc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", DecisionTreeClassifier(random_state=42))
-])
-
-xgbc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", XGBClassifier(random_state=42))
-])
-
-lgbmc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", LGBMClassifier(random_state=42))
-])
-
-cbc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", CatBoostClassifier(random_state=42))
-])
-
-gbc = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", GradientBoostingClassifier(random_state=42))
-])
-
-logreg = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", LogisticRegression(random_state=42))
-])
-
+# Regressors
 rfr = Pipeline([
     ("preprocessor", preprocessor),
     ("regressor", RandomForestRegressor(random_state=42))
