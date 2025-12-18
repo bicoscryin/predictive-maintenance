de4c669de5c927034b33bfe5df40075811cc87cf
bicoscryin <bico970@gmail.com>
2025-12-18 11:34:19 -0500

    cleaned up imports

--- src/all_imports.py
+++ src/all_imports.py
@@ -1,38 +1,19 @@
-import time
 import numpy as np
-from string import punctuation
 import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import seaborn as sns
-import sqlite3
 import math
-from math import ceil
-from collections import Counter
-from scipy import stats
-from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge, Lasso, RidgeCV, LassoCV
-from sklearn.cluster import KMeans
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingClassifier, RandomForestClassifier
-from sklearn.datasets import load_diabetes, load_iris, make_classification
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
-from sklearn.metrics import silhouette_score, mean_squared_error, r2_score, root_mean_squared_error, accuracy_score, precision_score, recall_score
-from sklearn.metrics import roc_curve, roc_auc_score, classification_report, confusion_matrix
+from sklearn.metrics import silhouette_score, mean_squared_error, r2_score, root_mean_squared_error
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor 
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import tensorflow as tf
-from tensorflow import keras
-from xgboost import XGBClassifier, XGBRegressor
-from lightgbm import LGBMClassifier
-from catboost import CatBoostClassifier
-import timeit
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
 import datetime
 import pickle
-import joblib
 import streamlit as st
 import plotly.express as px
